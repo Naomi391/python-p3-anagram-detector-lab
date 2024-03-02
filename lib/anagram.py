5874f2adb1e41fc3a0dd36a574bb0f7b7f396691
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-03-02 22:11:53 +0300

    Matched anagrams lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matched_anagrams = []
+        for other_word in word_list:
+            if sorted(self.word.lower()) == sorted(other_word.lower()):
+                matched_anagrams.append(other_word)
+        return matched_anagrams
